bfa7d18a3ab15d1d5f9780c0f51f3db7e05f5e9d
Maksim <maksim.lyutich@gmail.com>
2022-08-27 23:15:12 +0300

    edit url for predict

--- prediction_model/flask_app/predict.py
+++ prediction_model/flask_app/predict.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, request, jsonify
+from catboost import CatBoostRegressor
 
+import utils
 
 predict = Blueprint("predict", __name__)
 
 
 @predict.route('/predict', methods=['POST'])
 def predict_price():
-    pass
+    x_request = utils.get_data_object_from_request(request)
+
+    model = CatBoostRegressor()
+    model.load_model('catboost_regression')
+    pred = round(model.predict(x_request)[0], 1)
+
+    return jsonify({
+        "price": pred
+    })
